c81a006bbd84d2609c3546875e1dd896e8ec4dbb
ImJAraya <fabiarayamontero@gmail.com>
2025-03-21 17:14:38 -0600

    inclui la funcion del hacerDomainHack

--- src/app.js
+++ src/app.js
@@ -4,9 +4,9 @@
 //   }
 
 
-let pronoun = ['the', 'our'];
-let adj = ['great', 'big'];
-let noun = ['jogger', 'racoon'];
+let pronoun = ['the', 'our', 'my', 'your', 'his', 'her', 'its', 'their', 'this', 'that'];
+let adj = ['great', 'big', 'awesome', 'fast', 'crazy', 'funny', 'cool', 'epic', 'smart', 'weird'];
+let noun = ['jogger', 'racoon', 'developer', 'ninja', 'pirate', 'robot', 'dragon', 'wizard', 'unicorn', 'penguin'];
 let topLevelDomains = ['.com', '.net', '.us', '.io']
 
 
@@ -15,12 +15,17 @@ function posiblNomDominio() {
   
   let listaDeOpciones = [];
   let cont = 0;
+  let domainHack = true;
 
   for (let i = 0; i < pronoun.length; i++) {
     let nomDominioFinal = '';
     for (let j = 0; j < adj.length; j++) {
       for (let x = 0; x < noun.length; x++) {
-        nomDominioFinal += pronoun[i] +adj[j] +noun[x] + topLevelDomains[Math.floor(Math.random()*topLevelDomains.length)];
+        if ((Math.floor(Math.random()*2) == 1)){
+          nomDominioFinal += pronoun[i] +adj[j] + hacerDomainHack(noun[x]) + "  AQUI ESTA EL DOMAIN HACK"
+        } else {
+          nomDominioFinal += pronoun[i] +adj[j] +noun[x] + topLevelDomains[Math.floor(Math.random()*topLevelDomains.length)];
+        }
         listaDeOpciones.push(nomDominioFinal)
         cont++
         console.log(cont+" "+nomDominioFinal)
@@ -31,4 +36,19 @@ function posiblNomDominio() {
   return listaDeOpciones
 }
 
+function hacerDomainHack(palabra) {
+  let palabraTrucada = "";
+  for (let i = 0; i < palabra.length; i++) {
+    if (i  == palabra.length-3){
+      palabraTrucada+= palabra[i]+ "."
+    } else {
+      palabraTrucada+= palabra[i]
+    }
+  }
+  return palabraTrucada
+}
+
 posiblNomDominio()
+
+// let prueba = hacerDomainHack("palabra")
+// console.log(prueba)
